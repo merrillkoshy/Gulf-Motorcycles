c04ccd6a3fe4bbc7c898e1e7d67d875e9ba16682
merrillkoshy <merrillkoshy@gmail.com>
2021-05-31 13:48:10 +0400

    GA code edit

--- pages/_document.js
+++ pages/_document.js
@@ -92,17 +92,15 @@ class MyDocument extends Document {
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-197044504-1"`}
           />
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-197044504-1');
           `,
             }}
           />
