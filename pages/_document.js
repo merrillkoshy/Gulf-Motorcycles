591f56a76c145fecf93e87a7b2fd34f8ab193339
merrillkoshy <merrillkoshy@gmail.com>
2021-07-26 10:50:42 +0400

    fb pixel integration

--- pages/_document.js
+++ pages/_document.js
@@ -109,15 +109,15 @@ class MyDocument extends Document {
             dangerouslySetInnerHTML={{
               __html: `
               !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1159904417838017');
-              fbq('track', 'PageView');
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '537073047719554');
+  fbq('track', 'PageView');
               `
             }} />
 
@@ -164,7 +164,7 @@ class MyDocument extends Document {
             />
           </noscript>
           <noscript><img height="1" width="1" style={{ display: "none" }}
-            src={`https://www.facebook.com/tr?id=1159904417838017&ev=PageView&noscript=1`}
+            src={`https://www.facebook.com/tr?id=537073047719554&ev=PageView&noscript=1`}
           /></noscript>
           <Main />
           <NextScript />
